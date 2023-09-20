d3eb964218c28de440637803ee7b0b1ef2f0f542
Evgeniy Simonchik <y.simonchik@letyshops.com>
2023-09-20 17:57:55 +0400

    Change production API domain to api-sg.aliexpress.com

--- core/const.go
+++ core/const.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	// PRODUCT_GATEWAY 生产环境HTTPS地址
-	PRODUCT_GATEWAY = "https://eco.taobao.com/router/rest"
+	PRODUCT_GATEWAY = "https://api-sg.aliexpress.com/sync"
 
 	// SANDBOX_GATEWAY 箱环境HTTPS地址
 	SANDBOX_GATEWAY = "https://gw.api.tbsandbox.com/router/rest"
